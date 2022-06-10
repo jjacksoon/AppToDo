6dad21c5b7619d640634ef62ff2e3cb4c19b6170
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-06-10 07:53:14 -0300

    Adiciona tarefas com uso de modals

---
+++ components/InputModal.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Modal } from 'react-native';
+import {
+  ModalButton,
+  ModalContainer,
+  ModalView,
+  StyledInput,
+  ModalAction,
+  ModalActionGroup,
+  ModalIcon,
+  HeaderTitle,
+  colors
+} from './styles/appStyles';
+import {AntDesign} from '@expo/vector-icons';
+
+const InputModal = ({
+  modalVisible, 
+  setModalVisible, 
+  todoInputValue, 
+  setTodoInputValue, 
+  handleAddTodo, 
+  handleEditTodo,
+  todoToBeEdited,
+  setTodoToBeEdited,
+  todos
+}) => {
+  
+  }
+
+  return(
+    <>
+      <ModalButton
+        onPress={() => {setModalVisible(true)}}
+      >
+        <AntDesign name='plus' size={30} color={colors.secondary}/>
+      </ModalButton>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={handleCloseModal}
+      >
+        <ModalContainer>
+          <ModalView>
+            <ModalIcon>
+              <HeaderTitle>Todos</HeaderTitle>
+              <AntDesign name='edit' size={30} color={colors.tertiary}/>
+            </ModalIcon>
+
+            <StyledInput
+              placeholder="Insira uma tarefa"
+              placeholderTextColor={colors.alternative}
+              selectionColor={colors.secondary}
+              autoFocus={true}
+              onChangeText={(text) => setTodoInputValue(text)}
+              value={todoInputValue}
+              onSubmitEditing={handleSubmit}
+            />
+
+            <ModalActionGroup>
+              <ModalAction color={colors.primary} onPress={handleCloseModal}>
+                <AntDesign name='close' size={28} color={colors.tertiary}/>
+              </ModalAction>
+              <ModalAction  color={colors.tertiary} onPress={handleSubmit}>
+                <AntDesign name='check' size={28} color={colors.secondary}/>
+              </ModalAction>
+            </ModalActionGroup>
+          </ModalView>
+        </ModalContainer>
+      </Modal>
+    </>
+  );
+}
+export default InputModal;
